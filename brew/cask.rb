7bd2ace7cd4b27f955115047e3e5c8c526f00345
Tiago Rios <33334988+tiagojsrios@users.noreply.github.com>
2026-07-14 21:27:44 +0200

    Created brew's cask file

---
+++ brew/cask.rb
@@ -0,0 +1,11 @@
+cask "commuto" do
+  version "0.0.0"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+
+  url "https://placeholder"
+  name "Commuto"
+  desc "Commuto is a simple widget that shows when the next recommended commute trip will be."
+  homepage "https://github.com/tiagojsrios/commuto"
+
+  app "Commuto.app"
+end
