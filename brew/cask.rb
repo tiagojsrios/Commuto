5cc825e93e53b01cc28a83708d1bfcdea9712d4a
tiagojsrios <33334988+tiagojsrios@users.noreply.github.com>
2026-07-14 20:54:48 +0000

    Update commuto to v0.4.0

--- brew/cask.rb
+++ brew/cask.rb
@@ -1,8 +1,8 @@
 cask "commuto" do
-  version "0.0.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  version "0.4.0"
+  sha256 "3cbda228b0aa2f6f98317c06b71f6e7ae55cd35b95714917009edb6ebe24e59c"
 
-  url "https://placeholder"
+  url "https://github.com/tiagojsrios/Commuto/releases/download/v0.4.0/Commuto.zip"
   name "Commuto"
   desc "Commuto is a simple widget that shows when the next recommended commute trip will be."
   homepage "https://github.com/tiagojsrios/commuto"
